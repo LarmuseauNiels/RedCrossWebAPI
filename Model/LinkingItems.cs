50b859f93e5033e923d1329145a1e83e1463d4da
Nicolas Quarin <nicolas.quarin@outlook.com>
2020-06-12 14:53:19 +0200

    other removed

--- Model/LinkingItems.cs
+++ Model/LinkingItems.cs
@@ -10,30 +10,23 @@ namespace RedCrossBackend.Model
         public int id { get; set; }
         public int fAId { get; set; }
         public int aId { get; set; }
-        public string other { get; set; }
     }
     public class FAInjury
     {
         public int id { get; set; }
         public int fAId { get; set; }
         public int iId { get; set; }
-        public string other { get; set; }
-
     }
     public class FAPhType
     {
         public int id { get; set; }
         public int fAId { get; set; }
         public int pTId { get; set; }
-        public string other { get; set; }
-
     }
     public class FASetting
     {
         public int id { get; set; }
         public int fAId { get; set; }
         public int sId { get; set; }
-        public string other { get; set; }
-
     }
 }
