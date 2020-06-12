b707447109618cc6e6dd8b02c0cebfbb5ff1629d
Nicolas Quarin <nicolas.quarin@outlook.com>
2020-06-12 14:34:36 +0200

    Model from database

---
+++ Model/FirstAid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedCrossBackend.Model
+{
+    public class FirstAid
+    {
+        public int id { get; set; }
+        public DateTime assignDate { get; set; }
+        public string macAddress { get; set; }
+        public string gender { get; set; }
+        public string age { get; set; }
+        public string education { get; set; }
+        public bool hadFATraining { get; set; }
+        public int numberOffATraining { get; set; }
+        public bool trainingByRC { get; set; }
+        public bool blendedTraining { get; set; }
+        public string otherTrainingProvider { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public int confidentApplyingFA { get; set; }
+        public bool phNeeded { get; set; }
+        public int phTimeToArriveMs { get; set; }
+        public bool hospitalisationRequired { get; set; }
+    }
+}
---
+++ Model/Items.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedCrossBackend.Model
+{
+    public class Assistance
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    public class Setting
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    public class Injury
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    public class PhType
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
