4907660ddd4197537f1a2dd4fb7e26622cdc0f31
Nicolas Quarin <nicolas.quarin@outlook.com>
2020-06-13 20:10:59 +0200

    Controllers added

---
+++ Controllers/AnalyticsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedCrossBackend.DBModel;
+
+namespace RedCrossBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnalyticsController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+        private readonly DB_RedCrossContext context;
+
+        public AnalyticsController(ILogger<TestController> logger, DB_RedCrossContext dbContext)
+        {
+            _logger = logger;
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public Test Get()
+        {
+            var rng = new Random();
+            return new Test { example = "Basic Test" };
+        }
+    }
+}
---
+++ Controllers/AppController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedCrossBackend.DBModel;
+
+namespace RedCrossBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+        private readonly DB_RedCrossContext context;
+
+        public AppController(ILogger<TestController> logger, DB_RedCrossContext dbContext)
+        {
+            _logger = logger;
+            context = dbContext;
+        }
+
+        [HttpPost]
+        public bool SaveForm(FirstAid fa)
+        {
+            return true;
+        }
+    }
+}
